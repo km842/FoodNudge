4ccfb77f36231d31d7631fd5af889592d0ee8e06
Kunal <kunal@Kunals-MacBook-Pro.local>
2014-01-05 02:35:40 +0000

    Initial Commit

---
+++ FoodNudge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FoodNudge
+//
+//  Created by Kunal  on 05/01/2014.
+//  Copyright (c) 2014 Kunal . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
